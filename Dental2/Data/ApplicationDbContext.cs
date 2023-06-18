898fcd3801a035df0d667507849d0eefcbca5891
kawshik244 <kaisarulislam.kawshik@gmail.com>
2023-06-18 07:50:14 +0600

    feat: Authentication

---
+++ Dental2/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Dental2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dental2.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> User { set; get; }
+    }
+}
